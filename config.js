ec2703d192b29ce1203f9ec65830ecceefd30d90
botMaker96 <prateekbluestack63@gmail.com>
2025-03-27 10:01:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 module.exports = {
-    defaultPrefix: ';;',
-    ownerID: '',
+    defaultPrefix: ';@;',
+    ownerID: '<@&1354667871455674401>',
     _limits: 'The following are defaults.',
     adminCanChangeLimits: false,
     limits: {
         user_removals: {
-            per_minute: 0,
-            per_hour: 0
+            per_time: 1,
+            per_hour: 1
         },
         role_creations: {
-            per_minute: 0,
-            per_hour: 0
+            per_minute: 1,
+            per_hour: 2
         },
         channel_creations: {
-            per_minute: 0,
-            per_hour: 0
+            per_minute: 1,
+            per_hour: 2
         },
         role_deletions: {
-            per_minute: 0,
-            per_hour: 0
+            per_minute: 1,
+            per_hour: 2
         },
         channel_deletions: {
-            per_minute: 0,
-            per_hour: 0
+            per_minute: 1,
+            per_hour: 2
         },
         unbans: {
-            per_minute: 0,
-            per_hour: 0
+            per_minute: 1,
+            per_hour: 2
         },
         emoji_creations: {
             per_minute: 0,
